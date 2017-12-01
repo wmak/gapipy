c978d70ca823e2f8c6f7cd24a75efe7d0c50ed74
Ammaar Esmailjee <ammaar.esmailjee@gmail.com>
2017-12-01 12:25:08 -0500

    add simple cache for resource name -> resource lookup

--- gapipy/utils.py
+++ gapipy/utils.py
@@ -7,21 +7,29 @@ from functools import partial, wraps
 from importlib import import_module
 
 
+def get_available_resource_classes():
+    from .resources import available_resources
+    resource_module = import_module('gapipy.resources')
+    return [getattr(resource_module, r) for r in available_resources]
+
+
 def get_resource_class_from_class_name(name):
     resource_module = import_module('gapipy.resources')
     return getattr(resource_module, name)
 
 
+# simple cache to avoid computing the {resource_name -> resource} map
+# every time get_resource_class_from_resource_name is called
+__resource_name_mapping = None
+
 def get_resource_class_from_resource_name(name):
-    mapping = {resource._resource_name: resource
-               for resource in get_available_resource_classes()}
-    return mapping[name]
-
-
-def get_available_resource_classes():
-    from .resources import available_resources
-    resource_module = import_module('gapipy.resources')
-    return [getattr(resource_module, r) for r in available_resources]
+    if __resource_name_mapping is None:
+        __resource_name_mapping = {
+            resource._resource_name: resource
+            for resource in get_available_resource_classes()
+        }
+    # this will raise KeyError for invalid name
+    return __resource_name_mapping[name]
 
 
 def is_free(amount):
